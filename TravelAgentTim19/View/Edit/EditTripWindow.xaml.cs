79cf6bb96d743632fb0129c361ecb573e81934b0
Anita Pajic <anitaapajic@gmail.com>
2023-05-30 22:55:44 +0200

    Added map for attractions in trip info

--- TravelAgentTim19/View/Edit/EditTripWindow.xaml.cs
+++ TravelAgentTim19/View/Edit/EditTripWindow.xaml.cs
@@ -2,14 +2,19 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Media.Imaging;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsPresentation;
 using Microsoft.Win32;
 using NodaTime;
 using TravelAgentTim19.Model;
 using TravelAgentTim19.Repository;
+using Location = TravelAgentTim19.Model.Location;
 
 namespace TravelAgentTim19.View.Edit;
 
@@ -19,15 +24,77 @@ public partial class EditTripWindow : Window
     public Trip editTrip { get; set; }
     
     private MainRepository MainRepository;
+    public List<Location> AttractionsLocations { get; set; }
     public EditTripWindow(Trip trip, MainRepository mainRepository)
     {
         Trip = trip;
         // editTrip = new Trip(trip);
-        
+        AttractionsLocations = new List<Location>();
         MainRepository = mainRepository;
+        GetAttractionsLocation();
         InitializeComponent();
         DataContext = this;
 
+    }
+    public void GetAttractionsLocation()
+    {
+        foreach (Trip trip in MainRepository.TripRepository.GetTrips())
+        {
+            if (Trip.Id.Equals(trip.Id))
+            {
+                foreach (Attraction att in trip.Attractions)
+                {
+                    AttractionsLocations.Add(att.Location);
+                }
+            }
+        }
+        
+    }
+    private void MapControl_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+    {
+        if (e.ChangedButton == MouseButton.Left)
+        {
+            gmap.Zoom = (gmap.Zoom < gmap.MaxZoom) ? gmap.Zoom + 1 : gmap.MaxZoom;
+        }
+    }
+    private void map_load(object sender, RoutedEventArgs e)
+    {
+        gmap.Bearing = 0;
+        gmap.CanDragMap = true;
+        gmap.DragButton = MouseButton.Left;
+        gmap.MaxZoom = 18;
+        gmap.MinZoom = 2;
+        gmap.MouseWheelZoomType = MouseWheelZoomType.MousePositionWithoutCenter;
+    
+        gmap.ShowTileGridLines = false;
+        gmap.Zoom = 10;
+        gmap.ShowCenter = false;
+    
+        gmap.MapProvider = GMapProviders.GoogleMap;
+        GMaps.Instance.Mode = AccessMode.ServerOnly;
+        gmap.Position = new PointLatLng(Trip.Attractions[0].Location.Latitude, Trip.Attractions[0].Location.Longitude);
+    
+        GMapProvider.WebProxy = WebRequest.GetSystemWebProxy();
+        GMapProvider.WebProxy.Credentials = CredentialCache.DefaultCredentials;
+    
+        foreach (Location l in AttractionsLocations)
+        {
+            GMapMarker marker = new GMapMarker(new PointLatLng(l.Latitude, l.Longitude));
+            BitmapImage bi = new BitmapImage();
+            bi.BeginInit();
+            bi.UriSource = new Uri("pack://application:,,,/Images/redPin.png");
+            bi.EndInit();
+            Image pinImage = new Image();
+            pinImage.Source = bi;
+            pinImage.Width = 50; // Adjust as needed
+            pinImage.Height = 50; // Adjust as needed
+            pinImage.ToolTip = l.Address + " " + l.City;
+    
+            ToolTipService.SetShowDuration(pinImage, Int32.MaxValue);
+            ToolTipService.SetInitialShowDelay(pinImage, 0);
+            marker.Shape = pinImage;
+            gmap.Markers.Add(marker);
+        }
     }
 
     private void ClearData()
